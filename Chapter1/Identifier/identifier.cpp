98a87d03668f0bb5411576a8625d10e5e71c3bfd
Alec K <boltakziy@gmail.com>
2024-12-21 22:36:00 +0900

    Chapter 1

---
+++ Chapter1/Identifier/identifier.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Identifier should start with a character or an underbar." << endl;
+	cout << "It can not start with a number or a special symbol such as \'%\', \'$\', ....." << endl;
+	cout << "Identifier has case-sensitive." << endl;
+
+	return 0;
+}
