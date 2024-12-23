f6e26cb69e2b41111fd7ab5c439fd226c9ec17bb
Alec K <boltakziy@gmail.com>
2024-12-24 00:29:36 +0900

    Chapter 1

---
+++ Chapter1/String/String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str1 = "How much is the rate? : ", str2 = "How many is the count? : ";
+	double rate(0.05);
+	int count(3);
+
+	cout << str1 << rate << endl;
+	cout << str2 << count << endl;
+
+	return 0;
+}
