01202531ad24c1057578574ece7f9ac4c83a8483
Alec K <boltakziy@gmail.com>
2024-12-27 06:56:35 +0900

    Chapter 1

---
+++ Chapter1/LiteralVariable/LiteralVariable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double d = 1.57e-2;
+	char c = 'A';
+	string str = "En kanin og to katte";
+	bool b = true;
+
+	cout << "Real number : " << d << endl;
+	cout << "character : " << c << endl;
+	cout << "String : " << str << endl;
+	cout << "Boolean : " << b << endl;
+
+	return 0;
+}
