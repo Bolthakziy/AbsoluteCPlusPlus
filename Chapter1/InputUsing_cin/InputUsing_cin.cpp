5e3e1e489c3d1894bb4385d3e7a603e98ca1c1f9
Alec K <boltakziy@gmail.com>
2025-01-07 00:36:33 +0900

    Chapter 1

---
+++ Chapter1/InputUsing_cin/InputUsing_cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string firstName, lastName;
+
+	cout << "Input your full name. : ";
+	cin >> firstName >> lastName;
+	cout << "Your name is " << firstName << " " << lastName << endl;
+
+	return 0;
+}
